294ea22f3d7315190293d5eda8325542ad8b1b55
kaisalrashed <kaisalrashed3@gmail.com>
2025-05-24 12:37:46 +0300

    routes api

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,8 @@
 <?php
-
 use App\Http\Controllers\EventRequestController;
 use App\Http\Controllers\profileController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\VerificationController;
-use App\Http\Middleware\EnsureEmailIsVerified;
 use App\Http\Middleware\VerifyCodeRateLimit;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -14,32 +12,30 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::post('register', [UserController::class, 'register']);
-Route::post('login', [UserController::class, 'login']);
+route::post('register', [UserController::class, 'register']);
+route::post('login', [UserController::class, 'login']);
 
-Route::middleware(['auth:sanctum', EnsureEmailIsVerified::class])->group(function () {
+Route::middleware('auth:sanctum')->group(function () {
+    route::post('logout', [UserController::class, 'logout']);
 
-    Route::post('logout', [UserController::class, 'logout']);
+    route::apiResource('profiles', profileController::class);
 
-    Route::get('getAllUsers', [UserController::class, 'index'])->middleware('CheckUser');
-    Route::get('getUser/{id}', [UserController::class, 'show'])->middleware('CheckUser');
 
-    Route::apiResource('profiles', profileController::class);
-
-    Route::middleware('CheckProvider')->group(function () {
-        Route::get('getAllUsers', [UserController::class, 'index']);
-        Route::get('getUser/{id}', [UserController::class, 'show']);
-
-        Route::post('/event-requests', [EventRequestController::class, 'store']);
-    });
-
-    Route::middleware('auth:sanctum')->group(function () {
+    Route::middleware('verified')->group(function () {
         Route::post('send-verification-code', [VerificationController::class, 'send'])->middleware(VerifyCodeRateLimit::class);
-        Route::post('verify-verification-code', [VerificationController::class, 'verify']);
+        Route::middleware('CheckProvider')->group(function () {
+            route::get('getAllUsers', [UserController::class, 'index']);
+            route::get('getUser/{id}', [UserController::class, 'show']);
+            Route::post('/event-requests', [EventRequestController::class, 'store']);
+        });
     });
+
 
     Route::middleware('CheckAdmin')->group(function () {
         Route::get('/event-requests', [EventRequestController::class, 'index']);
         Route::put('/event-requests/{id}', [EventRequestController::class, 'update']);
     });
+
+
+    Route::post('verify-verification-code', [VerificationController::class, 'verify']);
 });
